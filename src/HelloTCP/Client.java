17f2377c7eca0987412e7bb3b8b681d88b1e2e0a
DocMop <hoffmann.philip@gmx.net>
2016-04-24 18:38:14 +0200

    First working version

--- src/HelloTCP/Client.java
+++ src/HelloTCP/Client.java
@@ -3,18 +3,66 @@
  */
 package HelloTCP;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 /**
  * @author pille
  *
  */
 public class Client {
-
+	private static final int defaultPort = 6789;
+	private static final String defaultServerAdress = "Localhost";
+	
+	public Client() {
+	}
+	
 	/**
-	 * @param args
+	 * @param adress Server adress
+	 * @param port Port number
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Client client = new Client();
+		
+		String serverAdress;
+		int port;
+		
+		if(args.length > 0)
+			serverAdress = args[0];
+		else
+			serverAdress = defaultServerAdress;
+			
+		if(args.length > 1)
+			port = Integer.parseInt(args[1]);
+		else
+			port = defaultPort;
+		
+		client.run(serverAdress, port);
+	}
+	
+	public void run(String serverAdress, int port) {
+		String outgoingMessage = "";
+		Scanner stdin = new Scanner(System.in);
+		
+		try {
+			while(!outgoingMessage.toLowerCase().trim().equals("exit")) {
+				Socket clientSocket = new Socket(serverAdress, port);
+				DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+				
+				System.out.println("Enter message to server or type 'exit' to close:");
+				outgoingMessage = stdin.nextLine();
+				
+				outToServer.writeBytes(outgoingMessage);
+				clientSocket.close();
+			}
+			
+			stdin.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
--- src/HelloTCP/Server.java
+++ src/HelloTCP/Server.java
@@ -3,18 +3,53 @@
  */
 package HelloTCP;
 
+import java.io.*;
+import java.net.*;
+
 /**
  * @author pille
  *
  */
 public class Server {
-
+	private static final int defaultPort = 6789;
+	
+	public Server() {
+	}
+	
 	/**
-	 * @param args
+	 * @param port Portnumber
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Server server = new Server();
+		
+		int port;
+		
+		if(args.length > 0)
+			port = Integer.parseInt(args[0]);
+		else
+			port = defaultPort;
+		
+		server.run(port);
 	}
 
+	private void run(int port) {
+		String incomingMessage;
+		ServerSocket welcomeSocket;
+		
+		try {
+			welcomeSocket = new ServerSocket(port);
+			
+			while(true) {
+				Socket connectionSocket = welcomeSocket.accept();
+				BufferedReader incomingStream =
+						new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+	
+				incomingMessage = incomingStream.readLine();
+				
+				System.out.println("RECEIVED: " + incomingMessage);
+			}			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
